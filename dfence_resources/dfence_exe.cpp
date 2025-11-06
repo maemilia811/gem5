4ae522009491bc84a88ab5454e7630196efad38d
maemilia811 <maemiliacaldara@mi.unc.edu.ar>
2025-11-06 20:32:57 -0300

    dev: clean comments and includes

--- dfence_resources/dfence_exe.cpp
+++ dfence_resources/dfence_exe.cpp
@@ -1,62 +1,27 @@
-#include <dirent.h>
-#include <m5_mmap.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include "dfence_exe.h"
 
-#include <cstring>
-#include <iostream>
-#include <string>
-#include <vector>
+int main(void){
 
-#include <gem5/m5ops.h>
+    uint32_t m5op_addr = 0XFFFF0000;
 
-int main() {
-
-    // Use the m5op_addr to input the "magic" address
-    m5op_addr = 0XFFFF0000;
-
-    // Use the map_m5_mem to map the "magic" address range to /dev/mem
     map_m5_mem();
 
-    printf("Terminó de mapearse la memoria\n");
-    // Use the gem5 m5ops to annotate the start of the ROI
+    printf("Finished memory mapping\n");
+
     m5_work_begin_addr(0, 0);
-    //ROI
+
     printf("Begin ROI\n");
 
     int result = system("/home/gem5/dfence_bin");
 
     if (result != 0) {
-        printf("Error al ejecutar dfence_bin. Codigo: %d\n", result);
+        printf("dfence_bin: Error, code: %d\n", result);
+        return result;
     }
 
-    //end ROI
-    // Use the gem5 m5ops to annotate the end of the ROI
     m5_work_end_addr(0, 0);
+
     printf(" End ROI\n");
 
-
-    // struct dirent *d;
-    // DIR *dr;
-    // dr = opendir(".");
-    // if (dr!=NULL) {
-    //     std::cout<<"List of Files & Folders:\n";
-    //     for (d=readdir(dr); d!=NULL; d=readdir(dr)) {
-    //         std::cout<<d->d_name<< ", ";
-    //     }
-    //     closedir(dr);
-    // }
-    // else {
-    //     std::cout<<"\nError Occurred!";
-    // }
-    // std::cout<<std::endl;
-
-    // Use unmap_m5_mem to unmap the "magic" address range
-    unmap_m5_mem();
-    printf("Terminó de mapeo de memoria \n");
-
-    // Terminar simulación
-    m5_exit(0);
-    printf("Despues del exit\n");
     return 0;
 }
