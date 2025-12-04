783fc31aab47e250b348e4e4b30d240ff03d3846
maemilia811 <maemiliacaldara@mi.unc.edu.ar>
2025-12-04 10:51:13 -0300

    dev: Agrego makefile y código de wrapper

---
+++ dfence_resources/security/spectre_wp.cpp
@@ -0,0 +1,69 @@
+#include "../dfence_exe.h"
+
+int main(void){
+
+    uint32_t m5op_addr = 0XFFFF0000;
+
+    map_m5_mem();
+
+    printf("Finished memory mapping\n");
+
+    m5_work_begin_addr(0, 0);
+
+    printf("Begin ROI\n");
+    #ifdef MFENCEV1
+        int result = system("/home/gem5/spectrev1_mfence");
+    #elif LFENCEV1
+        int result = system("/home/gem5/spectrev1_lfence");
+    #elif DFENCEV1
+        int result = system("/home/gem5/spectrev1_dfence");
+    #elif MFENCEV4
+        int result = system("/home/gem5/spectrev4_mfence");
+    #elif LFENCEV4
+        int result = system("/home/gem5/spectrev4_lfence");
+    #elif DFENCEV4
+        int result = system("/home/gem5/spectrev4_dfence");
+    #elif V1
+        int result = system("/home/gem5/spectrev1");
+    #elif V4
+        int result = system("/home/gem5/spectrev4");
+    #elif MFENCEV1_v2
+        int result = system("/home/gem5/spectrev1_mfence_v2");
+    #elif LFENCEV1_v2
+        int result = system("/home/gem5/spectrev1_lfence_v2");
+    #elif DFENCEV1_v2
+        int result = system("/home/gem5/spectrev1_dfence_v2");
+    #elif MFENCEV4_v2
+        int result = system("/home/gem5/spectrev4_mfence_v2");
+    #elif LFENCEV4_v2
+        int result = system("/home/gem5/spectrev4_lfence_v2");
+    #elif DFENCEV4_v2
+        int result = system("/home/gem5/spectrev4_dfence_v2");
+    #elif V1_v2
+        int result = system("/home/gem5/spectrev1_v2");
+    #elif V4_v2
+        int result = system("/home/gem5/spectrev4_v2");
+    #elif MFENCEV1_v3
+        int result = system("/home/gem5/spectrev1_mfence_v3");
+    #elif LFENCEV1_v3
+        int result = system("/home/gem5/spectrev1_lfence_v3");
+    #elif DFENCEV1_v3
+        int result = system("/home/gem5/spectrev1_dfence_v3");
+    #elif MFENCEV4_v3
+        int result = system("/home/gem5/spectrev4_mfence_v3");
+    #elif LFENCEV4_v3
+        int result = system("/home/gem5/spectrev4_lfence_v3");
+    #elif DFENCEV4_v3
+        int result = system("/home/gem5/spectrev4_dfence_v3");
+    #elif V1_v3
+        int result = system("/home/gem5/spectrev1_v3");
+    #elif V4_v3
+        int result = system("/home/gem5/spectrev4_v3");
+    #endif
+
+    m5_work_end_addr(0, 0);
+
+    printf(" End ROI\n");
+
+    return 0;
+}
