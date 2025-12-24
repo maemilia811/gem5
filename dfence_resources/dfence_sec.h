128d48fe4f7adb95c5e33ac352c3ee24904e27fc
maemilia811 <maemiliacaldara@mi.unc.edu.ar>
2025-12-24 12:04:18 -0300

    dev: Agrego .h para tests de seguridad de dfence

---
+++ dfence_resources/dfence_sec.h
@@ -0,0 +1,11 @@
+#include <m5_mmap.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+
+#include <gem5/m5ops.h>
+
+/*Basic libraries for dfence security tests*/
