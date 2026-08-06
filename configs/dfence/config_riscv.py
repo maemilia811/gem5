b2734004dbde9c0667c0fa0448f214433f44b3ad
maemilia811 <maemiliacaldara@mi.unc.edu.ar>
2026-08-06 23:27:38 +0200

    dev: add config files

---
+++ configs/dfence/config_riscv.py
@@ -0,0 +1,57 @@
+import argparse
+from pathlib import Path
+
+from O3_RISCV import *
+
+from m5.objects import *
+from m5.objects import RiscvO3CPU
+
+import gem5.simulate.simulator as Sim
+from gem5.components.boards.riscv_board import RiscvBoard
+from gem5.components.cachehierarchies.classic.private_l1_private_l2_walk_cache_hierarchy import (
+    PrivateL1PrivateL2WalkCacheHierarchy,
+)
+from gem5.components.memory.single_channel import SingleChannelDDR4_2400
+from gem5.components.processors.base_cpu_core import BaseCPUCore
+from gem5.components.processors.base_cpu_processor import BaseCPUProcessor
+from gem5.isas import ISA
+from gem5.resources.resource import BinaryResource
+
+parser = argparse.ArgumentParser(
+    description="Run a gem5 RISC-V simulation with a custom binary."
+)
+parser.add_init_argument = parser.add_argument(
+    "binary_path", type=str, help="Path to the RISC-V binary to execute"
+)
+args = parser.parse_args()
+
+cache_hierarchy = PrivateL1PrivateL2WalkCacheHierarchy(
+    l1d_size="32KiB",
+    l1i_size="32KiB",
+    l2_size="512KiB",
+)
+
+memory = SingleChannelDDR4_2400(size="3GiB")
+
+custom_cpu_instance = RiscvO3CPU()
+
+wrapped_core = BaseCPUCore(core=custom_cpu_instance, isa=ISA.RISCV)
+processor = BaseCPUProcessor(cores=[wrapped_core])
+
+for simple_core in processor.cores:
+    for i in range(len(simple_core.core.isa)):
+        simple_core.core.isa[i].riscv_type = "RV32"
+        simple_core.core.isa[i].enable_rvv = False
+
+
+board = RiscvBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+board.set_se_binary_workload(BinaryResource(local_path=args.binary_path))
+
+sim = Sim.Simulator(board=board)
+sim.run()
---
+++ configs/dfence/config_x86.py
@@ -0,0 +1,60 @@
+import argparse
+from pathlib import Path
+
+import gem5.components.processors.simple_processor as ProcSimple
+import gem5.components.processors.simple_processor as Proc
+import gem5.simulate.simulator as Sim
+from gem5.components.boards.x86_board import X86Board
+from gem5.components.cachehierarchies.classic.private_l1_private_l2_walk_cache_hierarchy import (
+    PrivateL1PrivateL2WalkCacheHierarchy,
+)
+from gem5.components.memory.single_channel import SingleChannelDDR4_2400
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.isas import ISA
+from gem5.resources.resource import (
+    BinaryResource,
+    obtain_resource,
+)
+
+parser = argparse.ArgumentParser(
+    description="Run a gem5 RISC-V simulation with a custom binary."
+)
+parser.add_init_argument = parser.add_argument(
+    "binary_path", type=str, help="Path to the X86 binary to execute"
+)
+args = parser.parse_args()
+
+# ---------- Define the system ----------
+cache_hierarchy = PrivateL1PrivateL2WalkCacheHierarchy(
+    l1d_size="16KiB",
+    l1i_size="16KiB",
+    l2_size="256KiB",
+)
+
+memory = SingleChannelDDR4_2400(size="3GiB")
+
+processor = Proc.SimpleProcessor(
+    cpu_type=CPUTypes.O3,
+    num_cores=1,
+    isa=ISA.X86,
+)
+
+board = X86Board(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+
+board.set_se_binary_workload(
+    binary=BinaryResource(local_path=args.binary_path)
+)
+
+sim = Sim.Simulator(
+    board=board,
+)
+
+sim.show_exit_event_messages()
+
+sim.run()
